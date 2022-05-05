9698ab757b460521e00fa27129fb4f3877cee2de
Thanh Nhan <hctxnhan@gmail.com>
2022-05-05 11:52:41 +0700

    Integrating redux-persist to persist redux-store

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,8 +1,19 @@
 import { compose, applyMiddleware, createStore } from 'redux';
 import rootReducer from './RootReducer';
 import logger from 'redux-logger';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [logger];
 const composeEnhancers = compose(applyMiddleware(...middleWares));
-const store = createStore(rootReducer, composeEnhancers);
-export default store;
+
+export const store = createStore(persistedReducer, composeEnhancers);
+export const persistor = persistStore(store);
